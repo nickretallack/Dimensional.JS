9ffc7f1c8f5a5fbd05851bbf1cbbe98e671f17ed
Nick Retallack <nickretallack@gmail.com>
2011-05-20 23:01:03 -0700

    added a few more units

--- example/main.js
+++ example/main.js
@@ -12,4 +12,5 @@ require([
     say("12cm * 30m + 1km^2 = " + Unit(50,'cm').times(Unit(30,'m')).plus(Unit(1, 'km^2')).show())
     say("5m/s * 52s = " + Unit(5,'m/s').times(Unit(52,'s')).show())
     say("1/(25m/s^2) = " + Unit(25,'m/s^').reciprocal().show())
+    say("0.1mi + 5ft + 20in + 300cm = " + Unit(0.1,'mi').plus(Unit(5,'ft')).plus(Unit(20,'in')).plus(Unit(300,'cm')).show())
 })
